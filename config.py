f308a89373ca14ee2e17b9ef0a9c2085be5eb267
Henrique Dias <heukirne@gmail.com>
2019-12-05 17:46:32 -0300

    update config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = getenv('SECRET_KEY') or 'insert_secret_key'
-    MYSQL_CONNECTION_STRING = ''
+    MYSQL_CONNECTION_STRING = 'mysql://username:password@server/db'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(
         minutes=int(getenv('JWT_ACCESS_TOKEN_EXPIRES', 20))
     )
