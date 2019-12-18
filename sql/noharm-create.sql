3a78f3680ac2c13d4319cef5257402ea37372500
Henrique Dias <heukirne@gmail.com>
2019-12-18 11:54:46 -0300

    update triggers and tables

--- sql/noharm-create.sql
+++ sql/noharm-create.sql
@@ -81,6 +81,8 @@ CREATE TABLE demo."presmed" (
   "fkmedicamento" integer NOT NULL,
   "fkunidademedida" varchar(10) DEFAULT NULL,
   "fkfrequencia" integer DEFAULT NULL,
+  "fkhospital" integer DEFAULT NULL,
+  "fksetor" integer DEFAULT NULL,
   "idsegmento" smallint DEFAULT NULL,
   "idoutlier" integer DEFAULT NULL,
   "dose" float DEFAULT NULL,
--- sql/noharm-triggers.sql
+++ sql/noharm-triggers.sql
@@ -1,64 +1,213 @@
+-------------------------------------
+-------- UPDATE SELF TABLES --------
+-------------------------------------
+
 CREATE FUNCTION demo.complete_presmed()
     RETURNS trigger
     LANGUAGE 'plpgsql'
     COST 100
     VOLATILE NOT LEAKPROOF
 AS $BODY$BEGIN
-    -- NEW.frequenciadia := por fkfrequencia
-    NEW.idoutlier := (
-        SELECT o.idoutlier FROM demo.outlier o WHERE 
-        o.fkmedicamento = NEW.fkmedicamento AND
-        o.dose = NEW.dose AND
-        o.frequenciadia = NEW.frequenciadia
+    NEW.frequenciadia := (
+        SELECT f.frequenciadia FROM frequencia f
+        WHERE f.fkfrequencia = NEW.fkfrequencia
     );
-    -- NEW.idsegmento := por fksetor + fkhospital
+    NEW.idoutlier := (
+        SELECT o.idoutlier FROM demo.outlier o 
+        WHERE o.fkmedicamento = NEW.fkmedicamento
+        AND o.dose = NEW.dose
+        AND o.frequenciadia = NEW.frequenciadia
+    );
+    NEW.idsegmento = (
+        SELECT s.idsegmento FROM segmentosetor s
+        WHERE s.fksetor = NEW.fksetor
+        AND s.fkhospital = NEW.fkhospital
+    );
     RETURN NEW;
 END;$BODY$;
 
 ALTER FUNCTION demo.complete_presmed()
     OWNER TO postgres;
 
-
-CREATE TRIGGER complete_presmed
+CREATE TRIGGER trg_complete_presmed
     BEFORE INSERT OR UPDATE 
     ON demo.presmed
     FOR EACH ROW
     EXECUTE PROCEDURE demo.complete_presmed();
 
-/*
-CREATE TRIGGER popula_prescricaoagg_by_segmento
-    AFTER INSERT OR UPDATE 
-    ON demo.segmentosetor
-    FOR EACH ROW
-    EXECUTE PROCEDURE demo.popula_prescricaoagg_by_segmento();
+--------
 
-CREATE TRIGGER complete_prescricao -- presmed.escorefinal, idsegmento
+CREATE FUNCTION demo.complete_prescricao()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$BEGIN
+    IF NEW.status = 'S' THEN
+        UPDATE demo.presmed pm
+        SET pm.escorefinal = (SELECT COALESCE(escoremanual, escore) 
+                                FROM demo.outlier o
+                                WHERE o.idoutlier = pm.idoutlier);
+    END IF;
+    NEW.idsegmento = (
+        SELECT s.idsegmento FROM segmentosetor s
+        WHERE s.fksetor = NEW.fksetor
+        AND s.fkhospital = NEW.fkhospital
+    );
+    RETURN NEW;
+END;$BODY$;
+
+ALTER FUNCTION demo.complete_prescricao()
+    OWNER TO postgres;
+
+CREATE TRIGGER trg_complete_prescricao
     BEFORE INSERT OR UPDATE 
     ON demo.prescricao
     FOR EACH ROW
     EXECUTE PROCEDURE demo.complete_prescricao();
 
-CREATE TRIGGER complete_prescricaoagg -- idsegmento, frequenciadia
+--------
+
+CREATE FUNCTION demo.complete_prescricaoagg()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$BEGIN
+    NEW.frequenciadia := (
+        SELECT f.frequenciadia FROM frequencia f
+        WHERE f.fkfrequencia = NEW.fkfrequencia
+    );
+    NEW.idsegmento = (
+        SELECT s.idsegmento FROM segmentosetor s
+        WHERE s.fksetor = NEW.fksetor
+        AND s.fkhospital = NEW.fkhospital
+    );
+    RETURN NEW;
+END;$BODY$;
+
+ALTER FUNCTION demo.complete_prescricaoagg()
+    OWNER TO postgres;
+
+CREATE TRIGGER trg_complete_prescricaoagg
     BEFORE INSERT OR UPDATE 
     ON demo.prescricaoagg
     FOR EACH ROW
     EXECUTE PROCEDURE demo.complete_prescricaoagg();
 
-CREATE TRIGGER popula_presmed_by_outlier
+--------
+
+CREATE FUNCTION demo.complete_frequencia()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$BEGIN
+    NEW.frequenciadia := 24 / NEW.frequenciahora;
+    RETURN NEW;
+END;$BODY$;
+
+ALTER FUNCTION demo.complete_frequencia()
+    OWNER TO postgres;
+
+CREATE TRIGGER trg_complete_frequencia
+    BEFORE INSERT OR UPDATE 
+    ON demo.frequencia
+    FOR EACH ROW
+    EXECUTE PROCEDURE demo.complete_frequencia();
+
+-------------------------------------
+-------- UPDATE CHILD TABLES --------
+-------------------------------------
+
+CREATE FUNCTION demo.popula_predmed_by_outlier()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$BEGIN
+    UPDATE presmed pm
+        SET pm.idoutlier = (
+            SELECT o.idoutlier FROM demo.outlier o 
+            WHERE o.fkmedicamento = pm.fkmedicamento
+            AND o.dose = pm.dose
+            AND o.frequenciadia = pm.frequenciadia
+        )
+    WHERE pm.escorefinal IS NULL;
+    RETURN NULL;
+END;$BODY$;
+
+ALTER FUNCTION demo.popula_predmed_by_outlier()
+    OWNER TO postgres;
+
+CREATE TRIGGER trg_popula_presmed_by_outlier
     AFTER INSERT OR UPDATE 
     ON demo.outlier
     FOR EACH ROW
     EXECUTE PROCEDURE demo.popula_predmed_by_outlier();
 
-CREATE TRIGGER popula_presmed_by_frequencia
+--------
+
+CREATE FUNCTION demo.popula_presmed_by_frequencia()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$BEGIN
+    UPDATE presmed pm
+        SET pm.frequenciadia = (
+            SELECT f.frequenciadia FROM demo.frequencia f 
+            WHERE o.fkfrequencia = pm.fkfrequencia
+        )
+    WHERE pm.escorefinal IS NULL;
+    RETURN NULL;
+END;$BODY$;
+
+ALTER FUNCTION demo.popula_presmed_by_frequencia()
+    OWNER TO postgres;
+
+CREATE TRIGGER trg_popula_presmed_by_frequencia
     AFTER INSERT OR UPDATE 
     ON demo.frequencia
     FOR EACH ROW
     EXECUTE PROCEDURE demo.popula_presmed_by_frequencia();
 
-CREATE TRIGGER complete_frequencia -- frequenciadia
-    BEFORE INSERT OR UPDATE 
-    ON demo.frequencia
+--------
+
+CREATE FUNCTION demo.popula_prescricaoagg_by_segmento()
+    RETURNS trigger
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE NOT LEAKPROOF
+AS $BODY$BEGIN
+    UPDATE presmed pm
+        SET pm.idsegmento = (
+            SELECT s.idsegmento FROM segmentosetor s
+            WHERE s.fksetor = pm.fksetor
+            AND s.fkhospital = pm.fkhospital
+        )
+    WHERE pm.escorefinal IS NULL;
+    UPDATE prescricao p
+        SET p.idsegmento = (
+            SELECT s.idsegmento FROM segmentosetor s
+            WHERE s.fksetor = p.fksetor
+            AND s.fkhospital = p.fkhospital
+        )
+    WHERE p.status IS NULL;
+    UPDATE prescricaoagg pa
+        SET pa.idsegmento = (
+            SELECT s.idsegmento FROM segmentosetor s
+            WHERE s.fksetor = pa.fksetor
+            AND s.fkhospital = pa.fkhospital
+        );
+    RETURN NULL;
+END;$BODY$;
+
+ALTER FUNCTION demo.popula_prescricaoagg_by_segmento()
+    OWNER TO postgres;
+
+CREATE TRIGGER trg_popula_prescricaoagg_by_segmento
+    AFTER INSERT OR UPDATE 
+    ON demo.segmentosetor
     FOR EACH ROW
-    EXECUTE PROCEDURE demo.complete_frequencia();
-*/
+    EXECUTE PROCEDURE demo.popula_prescricaoagg_by_segmento();
