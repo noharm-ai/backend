fb2fbe27a404182557e97fa719205ff5db9c1747
Henrique Dias <heukirne@gmail.com>
2019-12-26 13:56:05 -0300

    refactor outlier generation for distributed computation

--- routes/outlier_generate.py
+++ routes/outlier_generate.py
@@ -3,14 +3,17 @@ from flask_api import status
 from flask import Blueprint, request
 from multiprocessing import Process, Manager
 from models import db, User, setSchema, Outlier
-from flask_jwt_extended import (jwt_required, get_jwt_identity)
+from flask_jwt_extended import (jwt_required, get_jwt_identity, get_raw_jwt, create_access_token)
 from config import Config
 import pandas as pd
+from sqlalchemy import distinct, func
+from math import ceil
 
 app_gen = Blueprint('app_gen',__name__)
+fold_size = 7
 
-def compute_outlier(idDrug,drugsItem,poolDict):
-    print('Starting...', idDrug)
+def compute_outlier(idDrug, drugsItem, poolDict, fold):
+    print('Starting...', fold, idDrug)
     str_buffer = io.StringIO()
     drugsItem.to_csv(str_buffer, index=None)
 
@@ -18,25 +21,28 @@ def compute_outlier(idDrug,drugsItem,poolDict):
     with gzip.GzipFile(fileobj=gz_buffer, mode='wb') as zipped:
         zipped.write(bytes(str_buffer.getvalue(), 'utf-8'))
 
-    url = Config.DDC_API_URL + '/score'
-    files = {'file': gz_buffer.getvalue()}
-    data = {'userid':'1'}
+    if Config.DDC_API_URL != None:
+        url = Config.DDC_API_URL + '/score'
+        files = {'file': gz_buffer.getvalue()}
+        data = {'userid':'1'}
+        r = requests.post(url, files=files, data=data)
 
-    r = requests.post(url, files=files, data=data)
+        ungz_buffer = io.BytesIO()
+        ungz_buffer.write(r.content)
+        ungz_buffer.seek(0)
 
-    ungz_buffer = io.BytesIO()
-    ungz_buffer.write(r.content)
-    ungz_buffer.seek(0)
-
-    poolDict[idDrug] = pd.read_csv(ungz_buffer, compression='gzip')
-    print('End...', idDrug)
+        poolDict[idDrug] = pd.read_csv(ungz_buffer, compression='gzip')
+    print('End...', fold, idDrug)
 
 
 @app_gen.route("/segments/<int:idSegment>/outliers/generate", methods=['GET'])
 @jwt_required
-def generateOutliers(idSegment):
+def callOutliers(idSegment):
     user = User.find(get_jwt_identity())
     setSchema(user.schema)
+
+    auth_token = create_access_token(get_jwt_identity())
+    header = {'Authorization': 'Bearer ' + auth_token}
 
     conn = db.engine.raw_connection()
     cursor = conn.cursor()
@@ -53,10 +59,43 @@ def generateOutliers(idSegment):
     result = db.engine.execute(query)
     print('RowCount', result.rowcount)
 
+    totalCount = db.session.query(func.count(distinct(Outlier.idDrug)))\
+                .select_from(Outlier)\
+                .filter(Outlier.idSegment == idSegment)\
+                .scalar()
+    folds = ceil(totalCount / fold_size)
+    print('Total Count:', totalCount, folds)
+
+    for fold in range(1,folds+1):
+        url = Config.SELF_API_URL + "/segments/" + str(int(idSegment)) + "/outliers/generate/fold/" + str(fold)
+        print('Calling: ',url)
+        r = requests.get(url, headers=header)
+
+    return {
+        'status': 'success'
+    }, status.HTTP_200_OK
+
+@app_gen.route("/segments/<int:idSegment>/outliers/generate/fold/<int:fold>", methods=['GET'])
+@jwt_required
+def generateOutliers(idSegment,fold):
+    user = User.find(get_jwt_identity())
+    setSchema(user.schema)
+
+    conn = db.engine.raw_connection()
+    cursor = conn.cursor()
+
     query = "SELECT fkmedicamento as medication, dose, frequenciadia as frequency, SUM(contagem) as count \
           FROM " + user.schema + ".prescricaoagg \
           WHERE idsegmento = " + str(int(idSegment)) + " \
-          GROUP BY fkmedicamento, dose, frequenciadia"
+          GROUP BY fkmedicamento, dose, frequenciadia\
+          ORDER BY fkmedicamento\
+          LIMIT " + str(fold_size) + " OFFSET " + str((fold-1)*fold_size)
+
+    print(query)
+
+    return {
+        'status': 'success'
+    }, status.HTTP_200_OK
 
     outputquery = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(query)
 
@@ -71,7 +110,7 @@ def generateOutliers(idSegment):
     processes = []
     for idDrug in drugs['medication'].unique():
         drugsItem = drugs[drugs['medication']==idDrug]
-        process = Process(target=compute_outlier, args=(idDrug,drugsItem,poolDict,))
+        process = Process(target=compute_outlier, args=(idDrug,drugsItem,poolDict,fold,))
         processes.append(process)
 
     for process in processes:
@@ -82,21 +121,24 @@ def generateOutliers(idSegment):
 
     outliers = Outlier.query.filter(Outlier.idSegment == idSegment).all()
 
-    print('Appending Schema:', user.schema, 'Segment:', idSegment)
+    print('Appending Schema:', user.schema, 'Segment:', idSegment, 'Fold:', fold)
     new_os = pd.DataFrame()
-    for drug in poolDict:
-        new_os = new_os.append(poolDict[drug])
+    if Config.DDC_API_URL != None:
+        for drug in poolDict:
+            new_os = new_os.append(poolDict[drug])
 
-    print('Updating Schema:', user.schema, 'Segment:', idSegment)
-    for o in outliers:
-        no = new_os[(new_os['medication']==o.idDrug) & 
-                    (new_os['dose']==o.dose) &
-                    (new_os['frequency']==o.frequency)]
-        if len(no) > 0:
-            o.score = no['score'].values[0]
-            o.countNum = int(no['count'].values[0])
+    print('Updating Schema:', user.schema, 'Segment:', idSegment, 'Fold:', fold)
 
-    print('Commiting Schema:', user.schema, 'Segment:', idSegment)
+    if Config.DDC_API_URL != None:
+        for o in outliers:
+            no = new_os[(new_os['medication']==o.idDrug) & 
+                        (new_os['dose']==o.dose) &
+                        (new_os['frequency']==o.frequency)]
+            if len(no) > 0:
+                o.score = no['score'].values[0]
+                o.countNum = int(no['count'].values[0])
+
+    print('Commiting Schema:', user.schema, 'Segment:', idSegment, 'Fold:', fold)
     db.session.commit()
 
     return {
