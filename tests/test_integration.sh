de311173bf65464f9f0c667b8f1d383b9d57c928
Henrique Dias <heukirne@gmail.com>
2020-08-07 17:11:31 -0300

    add curl fail

--- tests/test_integration.sh
+++ tests/test_integration.sh
@@ -12,14 +12,14 @@ ADMISSION=5
 
 for LINK in reports patient-name/123 user/name-url outliers/${SEGMENT}/${DRUG} \
 			intervention/reasons intervention drugs/${SEGMENT} substance \
-			"prescriptions?idSegment=${SEGMENT}&date=2020-07-10" prescriptions/${PRESCRIPTION} \
+			"prescriptions?idSegment=${SEGMENT}&date=2020-12-31" prescriptions/${PRESCRIPTION} \
 			prescriptions/drug/${PRESCRIPTIONDRUG}/period \
-			/static/demo/prescription/${PRESCRIPTION} \
+			static/demo/prescription/${PRESCRIPTION} \
 			exams/${ADMISSION} segments segments/${SEGMENT} departments departments/free \
 			segments/exams/types \
-			/segments/${SEGMENT}/outliers/generate/drug/${DRUG} 
+			segments/${SEGMENT}/outliers/generate/drug/${DRUG} 
 do
-  COMMAND=("curl -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' '${HOST}/${LINK}'")
+  COMMAND=("curl --fail -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' '${HOST}/${LINK}'")
   printf "${LINK} "
   bash -c "${COMMAND}"
   printf "\n"
@@ -27,41 +27,41 @@ done
 
 LINK=("drugs/${DRUG}")
 DATA=('{ "idSegment": 1, "mav": true }')
-COMMAND=("curl -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
+COMMAND=("curl --fail -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
 printf "${LINK} "
 bash -c "${COMMAND}"
 printf "\n"
 
 LINK=("prescriptions/${PRESCRIPTION}")
 DATA=('{ "status": "s"}')
-COMMAND=("curl -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
+COMMAND=("curl --fail -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
 printf "${LINK} "
 bash -c "${COMMAND}"
 printf "\n"
 
 LINK=("patient/${ADMISSION}")
 DATA=('{ "height": 15}')
-COMMAND=("curl -X POST -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
+COMMAND=("curl --fail -X POST -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
 printf "${LINK} "
 bash -c "${COMMAND}"
 printf "\n"
 
 LINK=("prescriptions/drug/${PRESCRIPTIONDRUG}")
 DATA=('{ "height": 15}')
-COMMAND=("curl -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
+COMMAND=("curl --fail -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
 printf "${LINK} "
 bash -c "${COMMAND}"
 printf "\n"
 
 LINK=("prescriptions/drug/${PRESCRIPTIONDRUG}/1")
-COMMAND=("curl -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
+COMMAND=("curl --fail -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
 printf "${LINK} "
 bash -c "${COMMAND}"
 printf "\n"
 
 LINK=("intervention/${PRESCRIPTIONDRUG}")
 DATA=('{ "status": "s", "admissionNumber": 12832489}')
-COMMAND=("curl -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
+COMMAND=("curl --fail -X PUT -H 'Accept: application/json' -H 'Authorization: Bearer ${TOKEN}' -H 'Content-Type: application/json' ${HOST}/${LINK} -d '${DATA}'")
 printf "${LINK} "
 bash -c "${COMMAND}"
 printf "\n"
