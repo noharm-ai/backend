88aae831efbf8ea61f87809cf554ae810de9fb42
Henrique Dias <heukirne@gmail.com>
2020-10-08 22:55:17 -0300

    force action

--- mobile.py
+++ mobile.py
@@ -69,10 +69,7 @@ def add_security_headers(response):
             'frame-ancestors \'none\''
         ],
         'x-frame-options': ['SAMEORIGIN'],
-        'x-xss-protection': [
-            '1',
-            'mode=block'
-        ],
+        'x-xss-protection': [ '1', 'mode=block' ],
         'x-content-type-options': ['nosniff'],
         'referrer-policy': ['same-origin'],
     }
