c0cc9ef9486285ce1c4799ac262990b6a97091e5
Henrique Dias <heukirne@gmail.com>
2020-11-24 11:43:43 -0300

    bug fix aggsetor

--- models/prescription.py
+++ models/prescription.py
@@ -251,7 +251,7 @@ class Patient(db.Model):
 
         if (len(idDept)>0):
             idDept = list(map(int, idDept))
-            q = q.filter(postgresql.array(idDept).contains(Prescription.aggDeps))
+            q = q.filter(postgresql.array(idDept).overlap(Prescription.aggDeps))
 
         if (len(idDrug)>0):
             idDrug = list(map(int, idDrug))
