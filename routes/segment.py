0427822d0eb1fc4ed3d2cb70fca4296cbec6c622
Henrique Dias <heukirne@gmail.com>
2021-05-27 19:42:33 -0300

    improve exam type query

--- routes/segment.py
+++ routes/segment.py
@@ -178,7 +178,7 @@ def getCodes():
     dbSession.setSchema(user.schema)
 
     typesExam = db.session.query(Exams.typeExam)\
-                .filter(Exams.date > (date.today() - timedelta(days=360)))\
+                .filter(Exams.date > (date.today() - timedelta(days=90)))\
                 .group_by(Exams.typeExam)\
                 .order_by(asc(Exams.typeExam)).all()
 
