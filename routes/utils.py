0affbcb4b9066c609ab477aa7c2a4acb856174c9
Henrique Dias <heukirne@gmail.com>
2022-03-01 19:42:13 -0300

    fix ckd ajusted unit

--- routes/utils.py
+++ routes/utils.py
@@ -206,10 +206,13 @@ def ckd_calc(cr, birthdate, gender, skinColor, height, weight):
 
     eGFR = s * (float(cr)/g)**(e) * (0.993)**(age) if cr > 0 else 0
 
+    unit = 'ml/min/1.73'
+
     if is_float(height) and is_float(weight):
         eGFR *= math.sqrt((float(height) * float(weight)) / 3600) / (1.73)
+        unit = 'ml/min'
 
-    return { 'value': round(eGFR,1), 'ref': 'maior que 50 ml/min/1.73', 'unit': 'ml/min/1.73',
+    return { 'value': round(eGFR,1), 'ref': 'maior que 50 '+ unit, 'unit': unit,
              'alert': (eGFR < 50), 'name': 'Chronic Kidney Disease Epidemiology' , 
              'initials': 'CKD', 'min': 50, 'max': 120 }
 
