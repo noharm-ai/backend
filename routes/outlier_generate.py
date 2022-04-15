ce0ed7de30fdd8e2a74b37e13cba64ce4ce21517
Henrique Dias <heukirne@gmail.com>
2022-04-15 10:08:55 -0300

    improvements gen_outliers v4

--- routes/outlier_generate.py
+++ routes/outlier_generate.py
@@ -88,13 +88,13 @@ def generateOutliers(idSegment,fold=None,idDrug=None,clean=None):
         query += " AND fkmedicamento = " + str(int(idDrug))
 
         if clean != None:
-            queryUpdate = "UPDATE " + user.schema + ".presmed SET idoutlier = NULL WHERE fkmedicamento = " + str(int(idDrug)) \
-                                    + " AND idsegmento = " + str(int(idSegment)) \
-                                    + " AND fkprescricao in (SELECT fkprescricao FROM " + user.schema + ".prescricao" \
-                                                            + " WHERE dtprescricao > current_date - 2" \
-                                                            + " AND idsegmento = " + str(int(idSegment)) + ");"
-            result = db.engine.execute(queryUpdate)
-            print('RowCount Update Drug', result.rowcount)
+            #queryUpdate = "UPDATE " + user.schema + ".presmed SET idoutlier = NULL WHERE fkmedicamento = " + str(int(idDrug)) \
+            #                        + " AND idsegmento = " + str(int(idSegment)) \
+            #                        + " AND fkprescricao in (SELECT fkprescricao FROM " + user.schema + ".prescricao" \
+            #                                                + " WHERE dtprescricao > current_date - 2" \
+            #                                                + " AND idsegmento = " + str(int(idSegment)) + ");"
+            #result = db.engine.execute(queryUpdate)
+            #print('RowCount Update Drug', result.rowcount)
 
             queryDelete = "DELETE FROM " + user.schema + ".outlier WHERE fkmedicamento = " + str(int(idDrug)) + " AND idsegmento = " + str(int(idSegment)) + ";"
             result = db.engine.execute(queryDelete)
