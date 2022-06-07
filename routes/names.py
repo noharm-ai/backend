3a439149390306b23759ab221d20c7f1a7d0c584
Marcelo <marceloarocha@gmail.com>
2022-06-07 15:46:26 -0300

    proxy remove db connection

--- routes/names.py
+++ routes/names.py
@@ -1,21 +1,25 @@
+from os import getenv
 import requests
-from models.main import *
-from models.appendix import *
 from flask import Blueprint
-from flask_jwt_extended import (jwt_required, get_jwt_identity)
+from flask_jwt_extended import (jwt_required, get_jwt_identity, get_jwt)
 
 app_names = Blueprint('app_names',__name__)
 
 @app_names.route('/names/<int:idPatient>', methods=['GET'])
 @jwt_required()
 def proxy_name(idPatient):
-    user = User.find(get_jwt_identity())
-    config = Memory.getNameUrl(user.schema)
-    
-    url = config["to"].replace("{idPatient}", str(idPatient))
+    claims = get_jwt()
+    schema = claims["schema"].upper()
+    to_url = getenv(schema + '_PROXY_URL')
+    header_env = getenv(schema + '_PROXY_HEADERS')
+    headers = {}
+
+    for h in header_env.split("@@"):
+        item = h.split(":")
+        headers[item[0]] = item[1]
 
     response = requests.get(\
-        url=url,\
-        headers=config['headers'])
+        url=to_url.replace("{idPatient}", str(idPatient)),\
+        headers=headers)
     
     return response.json()
