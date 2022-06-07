88b461dcf980fd905466ad9aac04973d8b48fd96
Marcelo <marceloarocha@gmail.com>
2022-06-07 18:23:07 -0300

    debug logs

--- routes/names.py
+++ routes/names.py
@@ -3,12 +3,20 @@ from os import getenv
 import requests
 from flask import Blueprint
 from flask_jwt_extended import (jwt_required, get_jwt)
+import logging
+from http.client import HTTPConnection
 
 app_names = Blueprint('app_names',__name__)
 
 @app_names.route('/names/<int:idPatient>', methods=['GET'])
 @jwt_required()
 def proxy_name(idPatient):
+    HTTPConnection.debuglevel = 1
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
     claims = get_jwt()
     schema = claims["schema"].upper()
     to_url = getenv(schema + '_PROXY_URL')
@@ -23,5 +31,7 @@ def proxy_name(idPatient):
     response = requests.get(\
         url=to_url.replace("{idPatient}", str(idPatient)),\
         headers=headers)
+
+    current_app.logger.info('proxyresponse %s', response.content)
     
     return response.json()
