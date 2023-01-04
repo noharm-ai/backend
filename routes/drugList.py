de670fdd27f0a5b47dc0f13d67a4c8780e7d83cb
Marcelo <marceloarocha@gmail.com>
2023-01-03 21:12:08 -0300

    ira alert: fix counter

--- routes/drugList.py
+++ routes/drugList.py
@@ -28,7 +28,7 @@ class DrugList():
             }
 
     def sumAlerts(self):
-        self.alertStats['exams'] = self.alertStats['kidney'] + self.alertStats['liver'] + self.alertStats['platelets']
+        self.alertStats['exams'] = self.alertStats['exams'] + self.alertStats['kidney'] + self.alertStats['liver'] + self.alertStats['platelets']
         self.alertStats['allergy'] += self.alertStats['rea']
 
     @staticmethod
@@ -192,7 +192,7 @@ class DrugList():
                         maxira = 0.6219
 
                         if ira > maxira and self.dialysis is None:
-                            self.alertStats['platelets'] += 1
+                            self.alertStats['exams'] += 1
                             alerts.append('Risco de desenvolvimento de Insuficiência Renal Aguda (IRA), já que o resultado do cálculo [dose diária de VANCOMICINA/TFG/peso] é superior a 0,6219. Caso o paciente esteja em diálise, desconsiderar. Ref. em: https://revista.ghc.com.br/index.php/cadernosdeensinoepesquisa/issue/view/3')
 
             if self.is_cpoe:
