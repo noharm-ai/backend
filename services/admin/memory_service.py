9844404bcb1f3fa6b485ec948e021a3b5843fd9c
Marcelo <marceloarocha@gmail.com>
2023-05-11 20:37:00 -0300

    fix memory update

--- services/admin/memory_service.py
+++ services/admin/memory_service.py
@@ -12,17 +12,29 @@ def get_admin_memory_itens(user):
     if ('admin' not in roles):
         raise ValidationError('Usuário não autorizado', 'errors.unauthorizedUser', status.HTTP_401_UNAUTHORIZED)
     
+    kinds = ['features', 'reports', 'getnameurl']
+    
     memory_itens = db.session.query(Memory)\
-            .filter(Memory.kind.in_(['features', 'reports', 'getnameurl']))\
+            .filter(Memory.kind.in_(kinds))\
             .all()
     
     itens = []
+    foundKinds = []
     for i in memory_itens:
+        foundKinds.append(i.kind)
         itens.append({
             'key': i.key,
             'kind': i.kind,
             'value': i.value
         })
+
+    for k in kinds:
+        if k not in foundKinds:
+            itens.append({
+                'key': None,
+                'kind': k,
+                'value': []
+            })
 
     return itens
 
@@ -37,15 +49,16 @@ def update_memory(key, kind, value , user):
             .first()
     
     if (memory_item == None):
-        raise ValidationError('Registro inexistente', 'errors.invalidRecord', status.HTTP_400_BAD_REQUEST)
-    
-    #bkp
-    memory_bkp = Memory()
-    memory_bkp.kind = memory_item.kind + '_bkp'
-    memory_bkp.value = memory_item.value
-    memory_bkp.update = memory_item.update
-    memory_bkp.user = memory_item.user
-    db.session.add(memory_bkp)
+        memory_item = Memory()
+        memory_item.kind = kind
+    else:
+        #bkp
+        memory_bkp = Memory()
+        memory_bkp.kind = memory_item.kind + '_bkp'
+        memory_bkp.value = memory_item.value
+        memory_bkp.update = memory_item.update
+        memory_bkp.user = memory_item.user
+        db.session.add(memory_bkp)
 
     #update
     memory_item.value = value
