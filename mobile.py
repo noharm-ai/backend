8300dba27480d418cc49ebd0c079fe1d41873a07
Marcelo <marceloarocha@gmail.com>
2023-05-15 21:50:29 -0300

    v1.76-beta

--- mobile.py
+++ mobile.py
@@ -74,7 +74,7 @@ CORS(app)
 def getVersion():
     return {
         'status': 'success',
-        'data': 'v1.75-beta'
+        'data': 'v1.76-beta'
     }, status.HTTP_200_OK
 
 if __name__ == "__main__":
