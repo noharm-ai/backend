37e511e734d7728c4abea9eadcfe0eabddeecb64
Marcelo Arocha <marceloarocha@gmail.com>
2023-07-18 20:31:33 -0300

    v1.84-beta

--- mobile.py
+++ mobile.py
@@ -84,7 +84,7 @@ if Config.ENV == NoHarmENV.STAGING.value:
 
 @app.route("/version", methods=["GET"])
 def getVersion():
-    return {"status": "success", "data": "v1.83-beta"}, status.HTTP_200_OK
+    return {"status": "success", "data": "v1.84-beta"}, status.HTTP_200_OK
 
 
 if __name__ == "__main__":
