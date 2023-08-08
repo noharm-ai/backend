4d78f1827fa2b2933be734176470fa241114dfd5
Marcelo Arocha <marceloarocha@gmail.com>
2023-08-08 11:43:44 -0300

    summary: get annotations

--- models/notes.py
+++ models/notes.py
@@ -4,15 +4,16 @@ from sqlalchemy.orm import deferred
 from sqlalchemy import inspect, func, desc, distinct
 from datetime import datetime, timedelta
 
+
 class ClinicalNotes(db.Model):
-    __tablename__ = 'evolucao'
+    __tablename__ = "evolucao"
 
     id = db.Column("fkevolucao", db.Integer, primary_key=True)
-    admissionNumber = db.Column('nratendimento', db.Integer, nullable=False)
+    admissionNumber = db.Column("nratendimento", db.Integer, nullable=False)
     text = db.Column("texto", db.String, nullable=False)
-    date = db.Column('dtevolucao', db.DateTime, nullable=False)
-    prescriber = db.Column('prescritor', db.String, nullable=True)
-    position = db.Column('cargo', db.String, nullable=True)
+    date = db.Column("dtevolucao", db.DateTime, nullable=False)
+    prescriber = db.Column("prescritor", db.String, nullable=True)
+    position = db.Column("cargo", db.String, nullable=True)
     update = db.Column("update_at", db.DateTime, nullable=True)
     user = db.Column("update_by", db.Integer, nullable=True)
 
@@ -27,42 +28,64 @@ class ClinicalNotes(db.Model):
     dialysis = db.Column("dialise", db.Integer, nullable=True)
     names = db.Column("nomes", db.Integer, nullable=True)
 
-    signsText = db.Column('sinaistexto', db.String, nullable=True)
-    infoText = db.Column('dadostexto', db.String, nullable=True)
-    allergyText = db.Column('alergiatexto', db.String, nullable=True)
-    dialysisText = db.Column('dialisetexto', db.String, nullable=True)
+    signsText = db.Column("sinaistexto", db.String, nullable=True)
+    infoText = db.Column("dadostexto", db.String, nullable=True)
+    allergyText = db.Column("alergiatexto", db.String, nullable=True)
+    dialysisText = db.Column("dialisetexto", db.String, nullable=True)
 
     isExam = db.Column("exame", db.Boolean, nullable=False)
     # primary care columns
     form = deferred(db.Column("formulario", postgresql.JSON, nullable=True))
     template = deferred(db.Column("template", postgresql.JSON, nullable=True))
 
+    summary = deferred(db.Column("sumario", postgresql.JSONB, nullable=True))
+
     def exists():
-        tMap = db.session.connection()._execution_options.get("schema_translate_map", { None: None })
+        tMap = db.session.connection()._execution_options.get(
+            "schema_translate_map", {None: None}
+        )
         schemaName = tMap[None]
-        return db.engine.has_table('evolucao', schema=schemaName)
+        return db.engine.has_table("evolucao", schema=schemaName)
 
     def getCountIfExists(admissionNumber, pmc=False):
-        empty_return = [None,None,None,None,None,None,None,None,None,None,None]
+        empty_return = [
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+        ]
 
         if ClinicalNotes.exists():
-            qNotes = db.session.query(ClinicalNotes.admissionNumber, 
-                        func.sum(ClinicalNotes.medications),
-                        func.sum(ClinicalNotes.complication),
-                        func.sum(ClinicalNotes.symptoms),
-                        func.sum(ClinicalNotes.diseases),
-                        func.sum(ClinicalNotes.info),
-                        func.sum(ClinicalNotes.conduct),
-                        func.sum(ClinicalNotes.signs),
-                        func.sum(ClinicalNotes.allergy),
-                        func.count().label('total'),
-                        func.sum(ClinicalNotes.dialysis),
-                    ).select_from(ClinicalNotes)\
-                    .filter(ClinicalNotes.admissionNumber==admissionNumber)\
-                    .filter(ClinicalNotes.isExam == None)
+            qNotes = (
+                db.session.query(
+                    ClinicalNotes.admissionNumber,
+                    func.sum(ClinicalNotes.medications),
+                    func.sum(ClinicalNotes.complication),
+                    func.sum(ClinicalNotes.symptoms),
+                    func.sum(ClinicalNotes.diseases),
+                    func.sum(ClinicalNotes.info),
+                    func.sum(ClinicalNotes.conduct),
+                    func.sum(ClinicalNotes.signs),
+                    func.sum(ClinicalNotes.allergy),
+                    func.count().label("total"),
+                    func.sum(ClinicalNotes.dialysis),
+                )
+                .select_from(ClinicalNotes)
+                .filter(ClinicalNotes.admissionNumber == admissionNumber)
+                .filter(ClinicalNotes.isExam == None)
+            )
 
             if not pmc:
-                qNotes = qNotes.filter(ClinicalNotes.date > (datetime.today() - timedelta(days=6)))
+                qNotes = qNotes.filter(
+                    ClinicalNotes.date > (datetime.today() - timedelta(days=6))
+                )
 
             stats_return = qNotes.group_by(ClinicalNotes.admissionNumber).first()
 
@@ -73,11 +96,14 @@ class ClinicalNotes(db.Model):
 
     def getTotalIfExists(admissionNumber):
         if ClinicalNotes.exists():
-            qNotes = db.session.query(
-                        func.count().label('total'),
-                    ).select_from(ClinicalNotes)\
-                    .filter(ClinicalNotes.admissionNumber==admissionNumber)\
-                    .filter(ClinicalNotes.isExam == None)
+            qNotes = (
+                db.session.query(
+                    func.count().label("total"),
+                )
+                .select_from(ClinicalNotes)
+                .filter(ClinicalNotes.admissionNumber == admissionNumber)
+                .filter(ClinicalNotes.isExam == None)
+            )
 
             total = qNotes.scalar()
 
@@ -87,62 +113,80 @@ class ClinicalNotes(db.Model):
 
     def getComplicationCountIfExists(admissionNumber):
         if ClinicalNotes.exists():
-            return ClinicalNotes.query\
-                    .filter(ClinicalNotes.admissionNumber==admissionNumber)\
-                    .filter(ClinicalNotes.isExam == None)\
-                    .filter(ClinicalNotes.date > (datetime.today() - timedelta(days=6)))\
-                    .filter(ClinicalNotes.complication > 0)\
-                    .count()
+            return (
+                ClinicalNotes.query.filter(
+                    ClinicalNotes.admissionNumber == admissionNumber
+                )
+                .filter(ClinicalNotes.isExam == None)
+                .filter(ClinicalNotes.date > (datetime.today() - timedelta(days=6)))
+                .filter(ClinicalNotes.complication > 0)
+                .count()
+            )
         else:
             return None
 
     def getExamsIfExists(admissionNumber):
         if ClinicalNotes.exists():
-            return ClinicalNotes.query\
-                    .filter(ClinicalNotes.admissionNumber==admissionNumber)\
-                    .filter(ClinicalNotes.isExam == True)\
-                    .filter(ClinicalNotes.text != None)\
-                    .filter(ClinicalNotes.date > (datetime.today() - timedelta(days=90)))\
-                    .order_by(desc(ClinicalNotes.date))\
-                    .all()
+            return (
+                ClinicalNotes.query.filter(
+                    ClinicalNotes.admissionNumber == admissionNumber
+                )
+                .filter(ClinicalNotes.isExam == True)
+                .filter(ClinicalNotes.text != None)
+                .filter(ClinicalNotes.date > (datetime.today() - timedelta(days=90)))
+                .order_by(desc(ClinicalNotes.date))
+                .all()
+            )
         else:
             return []
 
     def getSigns(admissionNumber):
-        return db.session.query(ClinicalNotes.signsText, ClinicalNotes.date)\
-                .select_from(ClinicalNotes)\
-                .filter(ClinicalNotes.admissionNumber == admissionNumber)\
-                .filter(func.length(ClinicalNotes.signsText) > 0)\
-                .order_by(desc(ClinicalNotes.date))\
-                .first()
+        return (
+            db.session.query(ClinicalNotes.signsText, ClinicalNotes.date)
+            .select_from(ClinicalNotes)
+            .filter(ClinicalNotes.admissionNumber == admissionNumber)
+            .filter(func.length(ClinicalNotes.signsText) > 0)
+            .order_by(desc(ClinicalNotes.date))
+            .first()
+        )
 
     def getInfo(admissionNumber):
-        return db.session.query(ClinicalNotes.infoText, ClinicalNotes.date)\
-                .select_from(ClinicalNotes)\
-                .filter(ClinicalNotes.admissionNumber == admissionNumber)\
-                .filter(func.length(ClinicalNotes.infoText) > 0)\
-                .order_by(desc(ClinicalNotes.date))\
-                .first()
+        return (
+            db.session.query(ClinicalNotes.infoText, ClinicalNotes.date)
+            .select_from(ClinicalNotes)
+            .filter(ClinicalNotes.admissionNumber == admissionNumber)
+            .filter(func.length(ClinicalNotes.infoText) > 0)
+            .order_by(desc(ClinicalNotes.date))
+            .first()
+        )
 
     def getAllergies(admissionNumber):
-        return db.session.query(ClinicalNotes.allergyText, func.max(ClinicalNotes.date).label('maxdate'))\
-                .select_from(ClinicalNotes)\
-                .filter(ClinicalNotes.admissionNumber == admissionNumber)\
-                .filter(func.length(ClinicalNotes.allergyText) > 0)\
-                .group_by(ClinicalNotes.allergyText)\
-                .order_by(desc('maxdate'))\
-                .limit(50)\
-                .all()
+        return (
+            db.session.query(
+                ClinicalNotes.allergyText, func.max(ClinicalNotes.date).label("maxdate")
+            )
+            .select_from(ClinicalNotes)
+            .filter(ClinicalNotes.admissionNumber == admissionNumber)
+            .filter(func.length(ClinicalNotes.allergyText) > 0)
+            .group_by(ClinicalNotes.allergyText)
+            .order_by(desc("maxdate"))
+            .limit(50)
+            .all()
+        )
 
     def getDialysis(admissionNumber):
-        return db.session.query(\
-                    func.first_value(ClinicalNotes.dialysisText)\
-                        .over(partition_by=func.date(ClinicalNotes.date), order_by=desc(ClinicalNotes.date))\
-                    , func.date(ClinicalNotes.date).label('date')\
-                )\
-                .distinct(func.date(ClinicalNotes.date))\
-                .filter(ClinicalNotes.admissionNumber == admissionNumber)\
-                .filter(func.length(ClinicalNotes.dialysisText) > 0)\
-                .filter(ClinicalNotes.date > func.current_date() - 3)\
-                .order_by(desc('date'))\
-                .all()
+        return (
+            db.session.query(
+                func.first_value(ClinicalNotes.dialysisText).over(
+                    partition_by=func.date(ClinicalNotes.date),
+                    order_by=desc(ClinicalNotes.date),
+                ),
+                func.date(ClinicalNotes.date).label("date"),
+            )
+            .distinct(func.date(ClinicalNotes.date))
+            .filter(ClinicalNotes.admissionNumber == admissionNumber)
+            .filter(func.length(ClinicalNotes.dialysisText) > 0)
+            .filter(ClinicalNotes.date > func.current_date() - 3)
+            .order_by(desc("date"))
+            .all()
+        )
--- services/summary_service.py
+++ services/summary_service.py
@@ -1,15 +1,17 @@
 import json
 from flask_api import status
+from sqlalchemy import asc, desc, func, between
 
 from models.main import db
 from models.appendix import *
 from models.prescription import *
+from models.notes import *
 from models.enums import RoleEnum, MemoryEnum
 from services import memory_service, prescription_agg_service
 from exception.validation_error import ValidationError
 
 
-def get_structured_info(admission_number, user):
+def get_structured_info(admission_number, user, mock=False):
     roles = user.config["roles"] if user.config and "roles" in user.config else []
     # bloqueio temporario
     if RoleEnum.SUPPORT.value not in roles and RoleEnum.ADMIN.value not in roles:
@@ -43,7 +45,7 @@ def get_structured_info(admission_number, user):
             admission_number=admission_number, schema=user.schema
         ),
         "receipt": _get_receipt(admission_number=admission_number, schema=user.schema),
-        "summaryConfig": _get_summary_config(),
+        "summaryConfig": _get_summary_config(admission_number, mock),
     }
 
 
@@ -68,43 +70,55 @@ def _get_patient_data(patient: Patient):
     }
 
 
-def _get_summary_config():
+def _get_summary_config(admission_number, mock):
     summary_config = memory_service.get_memory(MemoryEnum.SUMMARY_CONFIG.value)
     # temporary source
-    reason = memory_service.get_memory("summary_text1")
-    previous_drugs = memory_service.get_memory("summary_text2")
-    diagnosis = memory_service.get_memory("summary_text_diagnosis")
-    discharge_condition = memory_service.get_memory("summary_text_dischargeCondition")
-    discharge_plan = memory_service.get_memory("summary_text_dischargePlan")
-    procedures = memory_service.get_memory("summary_text_procedures")
-    exams = memory_service.get_memory("summary_text_exams")
+    annotations = _get_all_annotations(admission_number)
+
+    if mock:
+        reason = memory_service.get_memory("summary_text1")
+        previous_drugs = memory_service.get_memory("summary_text2")
+        diagnosis = memory_service.get_memory("summary_text_diagnosis")
+        discharge_condition = memory_service.get_memory(
+            "summary_text_dischargeCondition"
+        )
+        discharge_plan = memory_service.get_memory("summary_text_dischargePlan")
+        procedures = memory_service.get_memory("summary_text_procedures")
+        exams = memory_service.get_memory("summary_text_exams")
 
     reason_payload = json.dumps(summary_config.value["reason"]).replace(
-        ":replace_text", reason.value["text"]
+        ":replace_text", reason.value["text"] if mock else annotations["reason"]
     )
 
     previous_drugs_payload = json.dumps(summary_config.value["previousDrugs"]).replace(
-        ":replace_text", previous_drugs.value["text"]
+        ":replace_text",
+        previous_drugs.value["text"] if mock else annotations["previousDrugs"],
     )
 
     diagnosis_payload = json.dumps(summary_config.value["diagnosis"]).replace(
-        ":replace_text", diagnosis.value["text"]
+        ":replace_text", diagnosis.value["text"] if mock else annotations["diagnosis"]
     )
 
     discharge_condition_payload = json.dumps(
         summary_config.value["dischargeCondition"]
-    ).replace(":replace_text", discharge_condition.value["text"])
+    ).replace(
+        ":replace_text",
+        discharge_condition.value["text"]
+        if mock
+        else annotations["dischargeCondition"],
+    )
 
     discharge_plan_payload = json.dumps(summary_config.value["dischargePlan"]).replace(
-        ":replace_text", discharge_plan.value["text"]
+        ":replace_text",
+        discharge_plan.value["text"] if mock else annotations["dischargePlan"],
     )
 
     procedures_payload = json.dumps(summary_config.value["procedures"]).replace(
-        ":replace_text", procedures.value["text"]
+        ":replace_text", procedures.value["text"] if mock else annotations["procedures"]
     )
 
     exams_payload = json.dumps(summary_config.value["exams"]).replace(
-        ":replace_text", exams.value["text"]
+        ":replace_text", exams.value["text"] if mock else annotations["exams"]
     )
 
     return {
@@ -118,6 +132,130 @@ def _get_summary_config():
         "procedures": json.loads(procedures_payload),
         "exams": json.loads(exams_payload),
     }
+
+
+def _get_all_annotations(admission_number):
+    first = (
+        db.session.query(ClinicalNotes)
+        .filter(ClinicalNotes.admissionNumber == admission_number)
+        .order_by(asc(ClinicalNotes.date))
+        .first()
+    )
+    last = (
+        db.session.query(ClinicalNotes)
+        .filter(ClinicalNotes.admissionNumber == admission_number)
+        .order_by(desc(ClinicalNotes.date))
+        .first()
+    )
+
+    reason = _get_annotation(
+        admission_number=admission_number,
+        field="motivo",
+        add=True,
+        interval="1 DAY",
+        compare_date=first.date,
+    )
+
+    previous_drugs = _get_annotation(
+        admission_number=admission_number,
+        field="medprevio",
+        add=True,
+        interval="1 DAY",
+        compare_date=first.date,
+    )
+
+    diagnosis = _get_annotation(
+        admission_number=admission_number,
+        field="diagnostico",
+        add=True,
+        interval=None,
+        compare_date=None,
+    )
+
+    clinical_summary = _get_annotation(
+        admission_number=admission_number,
+        field="resumo",
+        add=False,
+        interval="1 DAY",
+        compare_date=last.date,
+    )
+
+    discharge_plan = _get_annotation(
+        admission_number=admission_number,
+        field="planoalta",
+        add=False,
+        interval="1 DAY",
+        compare_date=last.date,
+    )
+
+    procedures = _get_annotation(
+        admission_number=admission_number,
+        field="procedimentos",
+        add=True,
+        interval=None,
+        compare_date=None,
+    )
+
+    exams = _get_annotation(
+        admission_number=admission_number,
+        field="exames",
+        add=True,
+        interval=None,
+        compare_date=None,
+    )
+
+    discharge_condition = _get_annotation(
+        admission_number=admission_number,
+        field="condicaoalta",
+        add=False,
+        interval="1 DAY",
+        compare_date=last.date,
+    )
+
+    return {
+        "reason": reason,
+        "previousDrugs": previous_drugs,
+        "diagnosis": diagnosis,
+        "clinicalSummary": clinical_summary,
+        "dischargePlan": discharge_plan,
+        "procedures": procedures,
+        "exams": exams,
+        "dischargeCondition": discharge_condition,
+    }
+
+
+def _get_annotation(admission_number, field, add, interval, compare_date):
+    query = (
+        db.session.query(func.jsonb_array_elements_text(ClinicalNotes.summary[field]))
+        .select_from(ClinicalNotes)
+        .filter(ClinicalNotes.admissionNumber == admission_number)
+    )
+
+    if compare_date:
+        if add:
+            query = query.filter(
+                between(
+                    func.date(ClinicalNotes.date),
+                    func.date(compare_date),
+                    func.date(compare_date) + func.cast(interval, INTERVAL),
+                )
+            )
+        else:
+            query = query.filter(
+                between(
+                    func.date(ClinicalNotes.date),
+                    func.date(compare_date) - func.cast(interval, INTERVAL),
+                    func.date(compare_date),
+                )
+            )
+
+    results = query.order_by(ClinicalNotes.date).all()
+
+    list = []
+    for i in results:
+        list.append(i[0])
+
+    return ". ".join(list)[:1500]
 
 
 def _get_exams(id_patient, schema):
