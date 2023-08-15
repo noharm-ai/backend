b87c995096996f27cf56b008aba75f1287aa283d
Marcelo Arocha <marceloarocha@gmail.com>
2023-08-15 16:41:39 -0300

    clinical notes: save draft

--- routes/memory.py
+++ routes/memory.py
@@ -1,56 +1,65 @@
 from models.main import *
 from models.appendix import *
 from flask import Blueprint, request
-from flask_jwt_extended import (jwt_required, get_jwt_identity)
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from .utils import tryCommit
-from datetime import datetime
 
-app_mem = Blueprint('app_mem',__name__)
+from services import memory_service
+from exception.validation_error import ValidationError
 
-@app_mem.route('/memory/<string:kind>', methods=['GET'])
+app_mem = Blueprint("app_mem", __name__)
+
+
+@app_mem.route("/memory/<string:kind>", methods=["GET"])
 @jwt_required()
 def getMemory(kind):
     user = User.find(get_jwt_identity())
     dbSession.setSchema(user.schema)
 
-    memList = Memory.query.filter(Memory.kind==kind).all()
+    if memory_service.is_admin_memory(kind):
+        return {
+            "status": "error",
+            "message": "Registro inválido",
+            "code": "errors.invalidRecord",
+        }, status.HTTP_400_BAD_REQUEST
+
+    memList = Memory.query.filter(Memory.kind == kind).all()
 
     results = []
     for m in memList:
-        results.append({
-            'key': m.key,
-            'value': m.value
-        })
-    
-    return {
-        'status': 'success',
-        'data': results
-    }, status.HTTP_200_OK
+        results.append({"key": m.key, "value": m.value})
 
-@app_mem.route('/memory/', methods=['PUT'])
-@app_mem.route('/memory/<int:idMemory>', methods=['PUT'])
+    return {"status": "success", "data": results}, status.HTTP_200_OK
+
+
+@app_mem.route("/memory/", methods=["PUT"])
+@app_mem.route("/memory/<int:idMemory>", methods=["PUT"])
 @jwt_required()
-def setSubstance(idMemory=None):
+def save_memory(idMemory=None):
     data = request.get_json()
     user = User.find(get_jwt_identity())
     dbSession.setSchema(user.schema)
 
-    newMem = False
-    if idMemory:
-        mem = Memory.query.get(idMemory)
-        if (mem is None):
-            return { 'status': 'error', 'message': 'Memória Inexistente!' }, status.HTTP_400_BAD_REQUEST
-    else:
-        newMem = True
-        mem = Memory()
+    try:
+        mem = memory_service.save_memory(
+            idMemory, data.get("type"), data.get("value"), user
+        )
+    except ValidationError as e:
+        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
 
-    mem.kind = data.get('type')
-    mem.value = data.get('value')
-    mem.update = datetime.today()
-    mem.user = user.id
+    return tryCommit(db, mem.key)
 
-    if newMem: 
-        db.session.add(mem)
-        db.session.flush()
+
+@app_mem.route("/memory/unique/<string:kind>", methods=["PUT"])
+@jwt_required()
+def save_memory_unique(kind):
+    data = request.get_json()
+    user = User.find(get_jwt_identity())
+    dbSession.setSchema(user.schema)
+
+    try:
+        mem = memory_service.save_unique_memory(kind, data.get("value"), user)
+    except ValidationError as e:
+        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
 
     return tryCommit(db, mem.key)
