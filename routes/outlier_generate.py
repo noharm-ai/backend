c6c58fdb2f0064b0e3241f01200d122b493795d1
Marcelo Arocha <marceloarocha@gmail.com>
2023-10-31 10:32:36 -0300

    refresh agg before generate score

--- routes/outlier_generate.py
+++ routes/outlier_generate.py
@@ -130,6 +130,18 @@ def generateOutliers(idSegment, fold=None, idDrug=None, clean=None):
             result = db.engine.execute(queryDelete)
             print("RowCount Delete Drug", result.rowcount)
 
+            queryRefresh = text(
+                f"""
+                    insert into {user.schema}.prescricaoagg
+                    select * from {user.schema}.prescricaoagg where fkmedicamento = :idDrug and idsegmento = :idSegment
+                """
+            )
+
+            result = db.engine.execute(
+                queryRefresh, {"idSegment": idSegment, "idDrug": idDrug}
+            )
+            print("RowCount Refresg Agg", result.rowcount)
+
         queryInsert = text(
             """
             INSERT INTO {schema}.outlier (idsegmento, fkmedicamento, doseconv, frequenciadia, contagem, escore, update_at)
