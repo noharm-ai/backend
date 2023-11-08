731f52f163bcb9aa30e2708972f590a536144461
Marcelo Arocha <marceloarocha@gmail.com>
2023-11-08 14:43:02 -0300

    add training permission

--- services/admin/frequency_service.py
+++ services/admin/frequency_service.py
@@ -3,23 +3,29 @@ from sqlalchemy import asc
 
 from models.main import db
 from models.appendix import *
+from models.enums import RoleEnum
 
 from exception.validation_error import ValidationError
 
+
 def get_frequencies():
-    return db.session\
-        .query(Frequency)\
-        .order_by(asc(Frequency.description))\
-        .all()
+    return db.session.query(Frequency).order_by(asc(Frequency.description)).all()
+
 
 def update_daily_frequency(id, daily_frequency, user):
-    roles = user.config['roles'] if user.config and 'roles' in user.config else []
-    if ('admin' not in roles):
-        raise ValidationError('Usuário não autorizado', 'errors.unauthorizedUser', status.HTTP_401_UNAUTHORIZED)
+    roles = user.config["roles"] if user.config and "roles" in user.config else []
+    if RoleEnum.ADMIN.value not in roles and RoleEnum.TRAINING.value not in roles:
+        raise ValidationError(
+            "Usuário não autorizado",
+            "errors.unauthorizedUser",
+            status.HTTP_401_UNAUTHORIZED,
+        )
 
     freq = Frequency.query.get(id)
-    if (freq is None):
-        raise ValidationError('Registro inexistente', 'errors.invalidRecord', status.HTTP_401_UNAUTHORIZED)
+    if freq is None:
+        raise ValidationError(
+            "Registro inexistente", "errors.invalidRecord", status.HTTP_401_UNAUTHORIZED
+        )
 
     freq.dailyFrequency = daily_frequency
 
@@ -28,15 +34,13 @@ def update_daily_frequency(id, daily_frequency, user):
 
     return freq
 
+
 def list_to_dto(frequencies):
     list = []
-    
+
     for p in frequencies:
-        list.append({
-            'id': p.id,
-            'name': p.description,
-            'dailyFrequency': p.dailyFrequency
-        })
+        list.append(
+            {"id": p.id, "name": p.description, "dailyFrequency": p.dailyFrequency}
+        )
 
     return list
-
--- services/admin/intervention_reason_service.py
+++ services/admin/intervention_reason_service.py
@@ -4,46 +4,61 @@ from sqlalchemy import asc, func
 from models.main import db
 from models.appendix import *
 from models.prescription import *
+from models.enums import RoleEnum
 
 from exception.validation_error import ValidationError
 
-def get_reasons(id = None, active_only = False):
-    parent = db.aliased(InterventionReason)
-    query_editable = db.session.\
-        query(func.count(Intervention.id))\
-        .select_from(Intervention)\
-        .filter(Intervention.idInterventionReason.any(InterventionReason.id))
 
-    q = db.session\
-        .query(\
-            InterventionReason,\
-            parent.description.label('parent_name'),\
-            func.concat(func.coalesce(parent.description, ''), InterventionReason.description).label('concat_field'),\
-            query_editable.exists().label('protected')
-        )\
-        .outerjoin(parent, InterventionReason.mamy == parent.id)\
-        .order_by(asc('concat_field'))
-    
+def get_reasons(id=None, active_only=False):
+    parent = db.aliased(InterventionReason)
+    query_editable = (
+        db.session.query(func.count(Intervention.id))
+        .select_from(Intervention)
+        .filter(Intervention.idInterventionReason.any(InterventionReason.id))
+    )
+
+    q = (
+        db.session.query(
+            InterventionReason,
+            parent.description.label("parent_name"),
+            func.concat(
+                func.coalesce(parent.description, ""), InterventionReason.description
+            ).label("concat_field"),
+            query_editable.exists().label("protected"),
+        )
+        .outerjoin(parent, InterventionReason.mamy == parent.id)
+        .order_by(asc("concat_field"))
+    )
+
     if id != None:
         q = q.filter(InterventionReason.id == id)
 
-    if (active_only):
+    if active_only:
         q = q.filter(InterventionReason.active == True)
 
     return q.all()
 
-def upsert_reason(id, reason: InterventionReason , user):
-    roles = user.config['roles'] if user.config and 'roles' in user.config else []
-    if ('admin' not in roles):
-        raise ValidationError('Usuário não autorizado', 'errors.unauthorizedUser', status.HTTP_401_UNAUTHORIZED)
-    
+
+def upsert_reason(id, reason: InterventionReason, user):
+    roles = user.config["roles"] if user.config and "roles" in user.config else []
+    if RoleEnum.ADMIN.value not in roles and RoleEnum.TRAINING.value not in roles:
+        raise ValidationError(
+            "Usuário não autorizado",
+            "errors.unauthorizedUser",
+            status.HTTP_401_UNAUTHORIZED,
+        )
+
     is_protected = False
-    
+
     if id != None:
         records = get_reasons(id)
-        if (len(records) == 0):
-            raise ValidationError('Registro inexistente', 'errors.invalidRecord', status.HTTP_400_BAD_REQUEST)
-        
+        if len(records) == 0:
+            raise ValidationError(
+                "Registro inexistente",
+                "errors.invalidRecord",
+                status.HTTP_400_BAD_REQUEST,
+            )
+
         record = records[0][0]
         is_protected = records[0][3]
     else:
@@ -64,21 +79,23 @@ def upsert_reason(id, reason: InterventionReason , user):
 
     return get_reasons(record.id)
 
+
 def list_to_dto(reasons):
     list = []
-    
+
     for r in reasons:
-        list.append({
-            'id': r[0].id,
-            'name': r[0].description,
-            'parentId': r[0].mamy,
-            'parentName': r[1],
-            'active': r[0].active,
-            'suspension': r[0].suspension,
-            'substitution': r[0].substitution,
-            'relationType': r[0].relation_type,
-            'protected': r[3]
-        })
+        list.append(
+            {
+                "id": r[0].id,
+                "name": r[0].description,
+                "parentId": r[0].mamy,
+                "parentName": r[1],
+                "active": r[0].active,
+                "suspension": r[0].suspension,
+                "substitution": r[0].substitution,
+                "relationType": r[0].relation_type,
+                "protected": r[3],
+            }
+        )
 
     return list
-
