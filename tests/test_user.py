914852455a2a39a0c7a0d8d83642243d8f6874ec
Marcelo Arocha <marceloarocha@gmail.com>
2024-01-04 15:01:24 -0300

    remove deprecated

--- tests/test_user.py
+++ tests/test_user.py
@@ -20,17 +20,6 @@ def create_user(client, data, access_token):
     )
 
 
-def test_get_reports(client):
-    """Teste get /reports/ - Valida status_code 200"""
-    access_token = get_access(client)
-
-    response = client.get("/reports", headers=make_headers(access_token))
-    data = json.loads(response.data)
-    # TODO: Add consulta ao banco de dados e comparar retorno (retornando status 200 porém data = [])
-
-    assert response.status_code == 200
-
-
 def test_get_users(client):
     """Teste get /users/ - Compara quantidade de usuários enviados com dados do banco e valida status_code 200"""
     access_token = get_access(client, roles=["userAdmin", "staging"])
