a86464f9908085ee08a24da192c2e0a6d820589a
Marcelo Arocha <marceloarocha@gmail.com>
2024-01-04 14:58:32 -0300

    remove deprecated

--- tests/test_basic_api.py
+++ tests/test_basic_api.py
@@ -20,24 +20,6 @@ def test_simple_get(client):
     assert response.status_code == 200
 
 
-def test_no_auth(client):
-    response = client.get("/reports")
-
-    assert response.status_code == 401
-
-
-@patch("models.main.User.find", side_effect=user_find)
-@patch("models.appendix.Memory.getMem", side_effect=getMem)
-@patch("models.main.dbSession.setSchema", side_effect=setSchema)
-def test_reports(user, memory, main, client):
-    response = client.get("/reports", headers=make_headers(access_token))
-    data = json.loads(response.data)
-
-    assert response.status_code == 200
-    assert data["status"] == "success"
-    assert data["reports"] == []
-
-
 def test_getInterventionReasons(client):
     """Teste get /intervention/reasons - Valida o status_code 200."""
 
