f5d0a50a8dd0da956b4da0ba3bc63cf911daffa1
Marcelo Arocha <marceloarocha@gmail.com>
2024-01-05 16:42:56 -0300

    search substance class

--- routes/substance.py
+++ routes/substance.py
@@ -3,6 +3,8 @@ from models.prescription import *
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from .utils import tryCommit
+
+from services import substance_service
 from exception.validation_error import ValidationError
 
 app_sub = Blueprint("app_sub", __name__)
@@ -58,25 +60,33 @@ def find_substance():
     user = User.find(get_jwt_identity())
     dbSession.setSchema(user.schema)
 
-    if term == "":
-        return {"status": "success", "data": []}, status.HTTP_200_OK
+    try:
+        results = substance_service.find_substance(term)
+    except ValidationError as e:
+        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
 
-    drugs = (
-        Substance.query.filter(Substance.name.ilike("%" + term + "%"))
-        .order_by(asc(Substance.name))
-        .all()
-    )
+    return {
+        "status": "success",
+        "data": results,
+    }, status.HTTP_200_OK
 
-    results = []
-    for d in drugs:
-        results.append(
-            {
-                "sctid": d.id,
-                "name": d.name.upper(),
-            }
-        )
 
-    return {"status": "success", "data": results}, status.HTTP_200_OK
+@app_sub.route("/substance/class/find", methods=["GET"])
+@jwt_required()
+def find_substance_class():
+    term = request.args.get("term", "")
+    user = User.find(get_jwt_identity())
+    dbSession.setSchema(user.schema)
+
+    try:
+        results = substance_service.find_substance_class(term)
+    except ValidationError as e:
+        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
+
+    return {
+        "status": "success",
+        "data": results,
+    }, status.HTTP_200_OK
 
 
 @app_sub.route("/substance/<int:idSubstance>", methods=["PUT"])
---
+++ services/substance_service.py
@@ -0,0 +1,65 @@
+from sqlalchemy import or_
+
+from models.main import db
+from models.appendix import *
+from models.prescription import *
+
+from exception.validation_error import ValidationError
+
+
+def find_substance(term):
+    if term == "" or term == None:
+        raise ValidationError(
+            "Busca inválida",
+            "errors.invalidParams",
+            status.HTTP_400_BAD_REQUEST,
+        )
+
+    items = (
+        Substance.query.filter(Substance.name.ilike("%" + term + "%"))
+        .order_by(asc(Substance.name))
+        .all()
+    )
+
+    results = []
+    for d in items:
+        results.append(
+            {
+                "sctid": d.id,
+                "name": d.name.upper(),
+            }
+        )
+
+    return results
+
+
+def find_substance_class(term):
+    if term == "" or term == None:
+        raise ValidationError(
+            "Busca inválida",
+            "errors.invalidParams",
+            status.HTTP_400_BAD_REQUEST,
+        )
+
+    parent = db.aliased(SubstanceClass)
+
+    items = (
+        db.session.query(
+            SubstanceClass,
+            parent.name.label("parent_name"),
+            func.concat(
+                func.coalesce(parent.name, ""), " - ", SubstanceClass.name
+            ).label("concat_field"),
+        )
+        .filter(
+            or_(SubstanceClass.name.ilike(f"%{term}%"), parent.name.ilike(f"%{term}%"))
+        )
+        .outerjoin(parent, SubstanceClass.idParent == parent.id)
+        .order_by(asc("concat_field"))
+    )
+
+    results = []
+    for i in items:
+        results.append({"id": i[0].id, "name": i[0].name, "parent": i[1]})
+
+    return results
