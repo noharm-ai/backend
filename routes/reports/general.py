167fdc316a538462ad6f25cfc601a6668fb494c9
Marcelo Arocha <marceloarocha@gmail.com>
2024-01-08 22:20:02 -0300

    report: audit

--- routes/reports/general.py
+++ routes/reports/general.py
@@ -54,3 +54,18 @@ def intervention():
         return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
 
     return tryCommit(db, report_data)
+
+
+@app_rpt_general.route("/reports/general/audit", methods=["GET"])
+@jwt_required()
+def audit():
+    user = User.find(get_jwt_identity())
+
+    try:
+        report_data = general_report_service.get_audit_report(
+            user=user, clearCache=request.args.get("clearCache", False)
+        )
+    except ValidationError as e:
+        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
+
+    return tryCommit(db, report_data)
