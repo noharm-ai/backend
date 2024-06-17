a0977241a9c7328902e26da1dd04437e65b24c77
Marcelo Arocha <marceloarocha@gmail.com>
2024-06-17 21:18:21 +0200

    show kit price even when suspendend

--- services/intervention_service.py
+++ services/intervention_service.py
@@ -828,7 +828,6 @@ def _get_price_kit(id_prescription, prescription_drug: PrescriptionDrug, user: U
                 PrescriptionDrug.cpoe_group == group,
             )
         )
-        .filter(PrescriptionDrug.suspendedDate == None)
         .all()
     )
 
