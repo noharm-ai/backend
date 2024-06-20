0273c1c709e47092e45c53d6a4840331a276ade2
Marcelo Arocha <marceloarocha@gmail.com>
2024-06-20 10:45:25 +0200

    cleanup findrelations

--- routes/prescription.py
+++ routes/prescription.py
@@ -363,14 +363,6 @@ def getPrescription(
         admissionNumber=patient.admissionNumber
     )
 
-    # relations = Prescription.findRelation(
-    #     prescription[0].id,
-    #     patient.admissionNumber,
-    #     patient.idPatient,
-    #     aggDate,
-    #     is_cpoe,
-    #     is_pmc,
-    # )
     relations = alert_service.find_relations(
         drug_list=drugs, is_cpoe=is_cpoe, id_patient=patient.idPatient
     )
