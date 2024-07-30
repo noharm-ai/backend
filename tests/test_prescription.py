2e83832295426eadb1f51ec628a60d28493fcb04
Rubens Mau <rubensmau@gmail.com>
2024-07-30 08:02:23 -0300

    New Unit Test by Rubens

--- tests/test_prescription.py
+++ tests/test_prescription.py
@@ -43,6 +43,27 @@ def test_get_prescriptions_by_idPrescription(client):
     assert data["bed"] == prescription.bed
     assert data["status"] == prescription.status
     assert len(data["prescription"]) > 0
+    
+
+
+def test_get_prescriptions_by_idPrescription_additional(client):
+    """Teste get /prescriptions/idPrescription - Compara response data com dados do banco e valida status_code 200
+       Additional idPrescription data validation"""
+
+    access_token = get_access(client)
+
+    idPrescription = "199"
+
+    response = client.get(
+        "/prescriptions/" + idPrescription, headers=make_headers(access_token)
+    )
+    data = json.loads(response.data)["data"]
+    
+    assert len(data['prescription']) == 6
+    assert len(data['solution']) == 0
+    assert len(data['procedures']) == 0
+    assert data['birthdate'] == '1941-02-05'
+
 
 
 def test_get_prescriptions_drug_by_idPrescription_and_period(client):
