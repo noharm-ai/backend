43a4ded38d71c1c8c9f3ca5c8de3360cd51e72e9
Rubens Mau <rubensmau@gmail.com>
2024-08-09 12:15:27 -0300

    2 conciliation tests added

--- tests/test_prescription.py
+++ tests/test_prescription.py
@@ -64,25 +64,6 @@ def test_get_prescriptions_by_idPrescription_additional(client):
     assert len(data['procedures']) == 0
     assert data['birthdate'] == '1941-02-05'
 
-def test_get_prescriptions_by_idPrescription_additional2(client):
-    """Teste get /prescriptions/idPrescription - Compara response data com dados do banco e valida status_code 200
-       Additional2 idPrescription data validation"""
-
-    access_token = get_access(client)
-
-    idPrescription = "9199"
-
-    response = client.get(
-        "/prescriptions/" + idPrescription, headers=make_headers(access_token)
-    )
-    data = json.loads(response.data)["data"]
-    
-    assert data['concilia'] == 's'
-    assert len(data['prescription']) == 1
-    assert data['prescription'][0]['drug'] == 'Medicamento do paciente'
-    assert len(data['solution']) == 0
-    assert len(data['procedures']) == 0
-    assert data['birthdate']   ==  "1941-02-05"
 
 
 def test_get_prescriptions_drug_by_idPrescription_and_period(client):
@@ -100,34 +81,6 @@ def test_get_prescriptions_drug_by_idPrescription_and_period(client):
 
     assert response.status_code == 200
 
-def test_putPrescriprionsCheck(client):
-    """Teste put /prescriptions/idPrescription - Checa o status "s" na prescrição e a existência de um resgistro na tabela prescricao_audit."""
-
-    url = f"/prescriptions/status"
-
-    access_token = get_access(client, roles=["staging"])
-
-    data = {"status": "s", "idPrescription": 9199}
-
-    response = client.post(
-        url, data=json.dumps(data), headers=make_headers(access_token)
-    )
-    
-    prescription = (
-        session.query(Prescription)
-        .filter(Prescription.id == 9199)
-        .filter(Prescription.status == "s")
-        .first()
-    )
-    prescriptionaudit = (
-        session.query(PrescriptionAudit)
-        .filter(PrescriptionAudit.idPrescription == 9199)
-        .filter(PrescriptionAudit.auditType == 1)
-        .first()
-    )
-    assert response.status_code == 200
-    assert prescription
-    assert prescriptionaudit
 
 def test_put_prescriptions_by_id(client):
     """Teste put /prescriptions/id - Compara dados enviados com dados salvos no banco e valida status_code 200"""
