ea0d00e3410aa84c1af8755ea8adee2d5b30203c
Marcelo Arocha <marceloarocha@gmail.com>
2024-09-11 18:35:21 -0300

    fix summary escape

--- services/summary_service.py
+++ services/summary_service.py
@@ -293,7 +293,7 @@ def _get_annotation(admission_number, field, add, interval, compare_date):
         uniqueList.add(i[0])
 
     return {
-        "value": ". ".join(uniqueList)[:2000].replace('"', '\\"'),
+        "value": ". ".join(uniqueList)[:2000].replace("\\", "").replace('"', '\\"'),
         "list": list(uniqueList),
     }
 
