7ca8a956c9ae91118458b37349729bf3e2cac5a3
Marcelo Arocha <marceloarocha@gmail.com>
2024-09-22 10:39:46 -0300

    refactor route using decorator wip

--- routes/admin/admin_integration_remote.py
+++ routes/admin/admin_integration_remote.py
@@ -1,15 +1,14 @@
-import os
 from flask import Blueprint, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from models.main import *
-from models.appendix import *
-from models.segment import *
-from models.prescription import *
+from decorators.api_endpoint_decorator import (
+    api_endpoint,
+    ApiEndpointUserGroup,
+    ApiEndpointAction,
+)
+from models.main import User
 from services.admin import (
     admin_integration_remote_service,
 )
-from exception.validation_error import ValidationError
 
 app_admin_integration_remote = Blueprint("app_admin_integration_remote", __name__)
 
@@ -17,99 +16,58 @@ app_admin_integration_remote = Blueprint("app_admin_integration_remote", __name_
 @app_admin_integration_remote.route(
     "/admin/integration-remote/template", methods=["GET"]
 )
-@jwt_required()
-def get_template():
-    user = User.find(get_jwt_identity())
-    dbSession.setSchema(user.schema)
-    os.environ["TZ"] = "America/Sao_Paulo"
-    try:
-        result = admin_integration_remote_service.get_template(
-            user=user,
-        )
-    except ValidationError as e:
-        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
-
-    return tryCommit(db, result)
+@api_endpoint(user_group=ApiEndpointUserGroup.ADMIN, action=ApiEndpointAction.READ)
+def get_template(user_context: User):
+    return admin_integration_remote_service.get_template(
+        user=user_context,
+    )
 
 
 @app_admin_integration_remote.route(
     "/admin/integration-remote/template-date", methods=["GET"]
 )
-@jwt_required()
-def get_template_date():
-    user = User.find(get_jwt_identity())
-    dbSession.setSchema(user.schema)
-    os.environ["TZ"] = "America/Sao_Paulo"
-    try:
-        result = admin_integration_remote_service.get_template_date(
-            user=user,
-        )
-    except ValidationError as e:
-        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
-
-    return tryCommit(db, result)
+@api_endpoint(user_group=ApiEndpointUserGroup.ADMIN, action=ApiEndpointAction.READ)
+def get_template_date(user_context: User):
+    return admin_integration_remote_service.get_template_date(
+        user=user_context,
+    )
 
 
 @app_admin_integration_remote.route(
     "/admin/integration-remote/set-state", methods=["POST"]
 )
-@jwt_required()
-def set_state():
-    user = User.find(get_jwt_identity())
-    dbSession.setSchema(user.schema)
-    os.environ["TZ"] = "America/Sao_Paulo"
+@api_endpoint(user_group=ApiEndpointUserGroup.ADMIN, action=ApiEndpointAction.WRITE)
+def set_state(user_context: User):
     request_data = request.get_json()
 
-    try:
-        result = admin_integration_remote_service.set_state(
-            id_processor=request_data.get("idProcessor", None),
-            state=request_data.get("state", None),
-            user=user,
-        )
-    except ValidationError as e:
-        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
-
-    return tryCommit(db, result)
+    return admin_integration_remote_service.set_state(
+        id_processor=request_data.get("idProcessor", None),
+        state=request_data.get("state", None),
+        user=user_context,
+    )
 
 
 @app_admin_integration_remote.route(
     "/admin/integration-remote/queue-status", methods=["GET"]
 )
-@jwt_required()
-def queue_status():
-    user = User.find(get_jwt_identity())
-    dbSession.setSchema(user.schema)
-    os.environ["TZ"] = "America/Sao_Paulo"
-
-    try:
-        result = admin_integration_remote_service.get_queue_status(
-            id_queue_list=request.args.getlist("idQueueList[]"),
-            user=user,
-        )
-    except ValidationError as e:
-        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
-
-    return tryCommit(db, result)
+@api_endpoint(user_group=ApiEndpointUserGroup.ADMIN, action=ApiEndpointAction.READ)
+def queue_status(user_context: User):
+    return admin_integration_remote_service.get_queue_status(
+        id_queue_list=request.args.getlist("idQueueList[]"),
+        user=user_context,
+    )
 
 
 @app_admin_integration_remote.route(
     "/admin/integration-remote/push-queue-request", methods=["POST"]
 )
-@jwt_required()
-def push_queue_request():
-    user = User.find(get_jwt_identity())
-    dbSession.setSchema(user.schema)
-    os.environ["TZ"] = "America/Sao_Paulo"
+@api_endpoint(user_group=ApiEndpointUserGroup.ADMIN, action=ApiEndpointAction.WRITE)
+def push_queue_request(user_context: User):
     request_data = request.get_json()
 
-    try:
-        result = admin_integration_remote_service.push_queue_request(
-            id_processor=request_data.get("idProcessor", None),
-            action_type=request_data.get("actionType", None),
-            data=request_data,
-            user=user,
-        )
-    except ValidationError as e:
-        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
-
-    return tryCommit(db, result)
+    return admin_integration_remote_service.push_queue_request(
+        id_processor=request_data.get("idProcessor", None),
+        action_type=request_data.get("actionType", None),
+        data=request_data,
+        user=user_context,
+    )
