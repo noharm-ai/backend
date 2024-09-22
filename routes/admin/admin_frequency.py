64d324ce4127affd901716fc2de4560336290d2c
Marcelo Arocha <marceloarocha@gmail.com>
2024-09-22 10:32:20 -0300

    refactor route using decorator wip

--- routes/admin/admin_frequency.py
+++ routes/admin/admin_frequency.py
@@ -1,51 +1,40 @@
-import os
 from flask import Blueprint, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from utils import status
-from models.main import *
-from models.appendix import *
-from models.segment import *
-from models.prescription import *
+from decorators.api_endpoint_decorator import (
+    api_endpoint,
+    ApiEndpointUserGroup,
+    ApiEndpointAction,
+)
+from models.main import User
 from services.admin import admin_frequency_service
-from exception.validation_error import ValidationError
 
 app_admin_freq = Blueprint("app_admin_freq", __name__)
 
 
 @app_admin_freq.route("/admin/frequency/list", methods=["POST"])
-@jwt_required()
+@api_endpoint(user_group=ApiEndpointUserGroup.MAINTAINER, action=ApiEndpointAction.READ)
 def get_frequencies():
-    user = User.find(get_jwt_identity())
-    dbSession.setSchema(user.schema)
     request_data = request.get_json()
 
     list = admin_frequency_service.get_frequencies(
         has_daily_frequency=request_data.get("hasDailyFrequency", None),
     )
 
-    return {
-        "status": "success",
-        "data": admin_frequency_service.list_to_dto(list),
-    }, status.HTTP_200_OK
+    return admin_frequency_service.list_to_dto(list)
 
 
 @app_admin_freq.route("/admin/frequency", methods=["PUT"])
-@jwt_required()
-def update_frequency():
+@api_endpoint(
+    user_group=ApiEndpointUserGroup.MAINTAINER, action=ApiEndpointAction.WRITE
+)
+def update_frequency(user_context: User):
     data = request.get_json()
-    user = User.find(get_jwt_identity())
-    dbSession.setSchema(user.schema)
-    os.environ["TZ"] = "America/Sao_Paulo"
 
-    try:
-        freq = admin_frequency_service.update_frequency(
-            id=data.get("id", None),
-            daily_frequency=data.get("dailyFrequency", None),
-            fasting=data.get("fasting", None),
-            user=user,
-        )
-    except ValidationError as e:
-        return {"status": "error", "message": str(e), "code": e.code}, e.httpStatus
+    freq = admin_frequency_service.update_frequency(
+        id=data.get("id", None),
+        daily_frequency=data.get("dailyFrequency", None),
+        fasting=data.get("fasting", None),
+        user=user_context,
+    )
 
-    return tryCommit(db, admin_frequency_service.list_to_dto([freq]))
+    return admin_frequency_service.list_to_dto([freq])
