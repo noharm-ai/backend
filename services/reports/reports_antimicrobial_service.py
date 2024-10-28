62ee3a40b8b70a6e92eeb28cd5d3ed81a61ade49
Marcelo Arocha <marceloarocha@gmail.com>
2024-10-28 20:19:17 -0300

    reports antimicrobial perf fix

--- services/reports/reports_antimicrobial_service.py
+++ services/reports/reports_antimicrobial_service.py
@@ -1,15 +1,8 @@
-from sqlalchemy import select, and_, desc
+from sqlalchemy import select, desc, text
 
-from models.main import db
+from models.main import db, User
 from models.prescription import (
     Patient,
-    Prescription,
-    PrescriptionDrug,
-    Drug,
-    DrugAttributes,
-    Frequency,
-    MeasureUnit,
-    Substance,
 )
 from exception.validation_error import ValidationError
 from utils import dateutils, status
@@ -17,15 +10,15 @@ from decorators.has_permission_decorator import has_permission, Permission
 
 
 @has_permission(Permission.READ_REPORTS)
-def get_history(admission_number: int):
+def get_history(admission_number: int, user_context: User):
     if admission_number == None:
         raise ValidationError(
-            "admissionNUmber inválido",
+            "admissionNmber inválido",
             "errors.invalidParams",
             status.HTTP_400_BAD_REQUEST,
         )
 
-    admission_list = [admission_number]
+    admission_list = [int(admission_number)]
     admission = db.session.execute(
         select(Patient.idPatient)
         .select_from(Patient)
@@ -44,43 +37,46 @@ def get_history(admission_number: int):
 
         a_list = db.session.execute(admissions_query).all()
         for a in a_list:
-            admission_list.append(a.admissionNumber)
+            admission_list.append(int(a.admissionNumber))
 
-    query = (
-        select(
-            Prescription.id,
-            Prescription.date,
-            Prescription.expire,
-            Prescription.admissionNumber,
-            PrescriptionDrug.suspendedDate,
-            PrescriptionDrug.id.label("idPrescriptionDrug"),
-            PrescriptionDrug.dose,
-            PrescriptionDrug.route,
-            Drug.name,
-            Substance.name.label("substance"),
-            MeasureUnit.description.label("measureUnit"),
-            Frequency.description.label("frequency"),
-        )
-        .select_from(Prescription)
-        .join(PrescriptionDrug, PrescriptionDrug.idPrescription == Prescription.id)
-        .join(Drug, Drug.id == PrescriptionDrug.idDrug)
-        .join(
-            DrugAttributes,
-            and_(
-                DrugAttributes.idDrug == PrescriptionDrug.idDrug,
-                DrugAttributes.idSegment == PrescriptionDrug.idSegment,
-            ),
-        )
-        .outerjoin(Substance, Drug.sctid == Substance.id)
-        .outerjoin(Frequency, Frequency.id == PrescriptionDrug.idFrequency)
-        .outerjoin(MeasureUnit, MeasureUnit.id == PrescriptionDrug.idMeasureUnit)
-        .where(Prescription.admissionNumber.in_(admission_list))
-        .where(DrugAttributes.antimicro == True)
-        .order_by(desc(Prescription.date))
-        .limit(2000)
+    query = text(
+        f"""
+            select
+                antimicro.*,
+                m.nome as name,
+                s.nome as substance 
+            from (
+                SELECT
+                    p.fkprescricao as id,
+                    p.dtprescricao as date,
+                    p.dtvigencia as expire,
+                    p.nratendimento as "admissionNumber",
+                    pm.dtsuspensao as "suspendedDate",
+                    pm.fkpresmed AS "idPrescriptionDrug",
+                    pm.dose as dose,
+                    pm.via as route,
+                    pm.fkmedicamento,
+                    um.nome AS "measureUnit",
+                    f.nome AS frequency
+                FROM
+                    {user_context.schema}.prescricao p
+                    JOIN {user_context.schema}.presmed pm ON pm.fkprescricao = p.fkprescricao
+                    JOIN {user_context.schema}.medatributos ma ON  ma.fkmedicamento = pm.fkmedicamento AND ma.idsegmento = pm.idsegmento
+                    LEFT OUTER JOIN {user_context.schema}.frequencia f ON f.fkfrequencia = pm.fkfrequencia
+                    LEFT OUTER JOIN {user_context.schema}.unidademedida um ON um.fkunidademedida = pm.fkunidademedida
+                WHERE
+                    p.nratendimento = ANY(:admissionList)
+                    AND ma.antimicro = TRUE
+                ORDER BY
+                    p.dtprescricao desc
+                limit 2000
+            ) antimicro
+            JOIN {user_context.schema}.medicamento m ON antimicro.fkmedicamento = m.fkmedicamento
+            LEFT OUTER JOIN public.substancia s ON m.sctid = s.sctid
+        """
     )
 
-    results = db.session.execute(query).all()
+    results = db.session.execute(query, {"admissionList": admission_list}).all()
     items = []
 
     for row in results:
