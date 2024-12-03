c8589414beeee93648a582e2cf7cac4e57212d7c
Henrique Dias <heukirne@gmail.com>
2024-12-03 11:27:48 -0300

    add llama 3.1 450b

--- services/llm_service.py
+++ services/llm_service.py
@@ -26,6 +26,7 @@ def prompt(messages, options={}):
         summary_config.value["provider"] != "openai_azure"
         and summary_config.value["provider"] != "claude"
         and summary_config.value["provider"] != "maritaca"
+        and summary_config.value["provider"] != "llama"
     ):
         raise ValidationError(
             "Configuração inválida",
@@ -40,6 +41,9 @@ def prompt(messages, options={}):
         return _prompt_maritaca(messages=messages, options=options)
 
     if summary_config.value["provider"] == "claude":
+        return _prompt_claude(messages=messages)
+
+    if summary_config.value["provider"] == "llama":
         return _prompt_claude(messages=messages)
 
 
@@ -90,3 +94,32 @@ def _prompt_claude(messages):
     response_body = json.loads(response.get("body").read())
 
     return {"answer": response_body["content"][0]["text"]}
+
+def _prompt_llama(messages):
+    session = boto3.session.Session()
+    client = session.client("bedrock-runtime", region_name="us-west-2")
+
+    prompt = "<|begin_of_text|>"
+    for m in messages:
+        prompt += '<|start_header_id|>' + m['role'] + '<|end_header_id|>\n'
+        prompt += m['content'] + '<|eot_id|>\n'
+    prompt += '<|start_header_id|>assistant<|end_header_id|>'
+
+    body = json.dumps({
+        "prompt": prompt, 
+        "max_gen_len":1024,
+        "temperature":0.5,
+        "top_p":0.9
+    })
+
+    modelId = "meta.llama3-1-405b-instruct-v1:0"
+    accept = "application/json"
+    contentType = "application/json"
+
+    response = client.invoke_model(
+        body=body, modelId=modelId, accept=accept, contentType=contentType
+    )
+
+    response_body = json.loads(response.get("body").read())
+
+    return {"answer": response_body['generation']}
