d7e9efe518e5f0d72eb2c8a9a243bbe3b9847e98
Arthur Tabbal <arthurtabbal@gmail.com>
2024-12-17 09:24:30 -0300

    Refactor: adjust imports

--- services/reports/reports_exams_service.py
+++ services/reports/reports_exams_service.py
@@ -1,7 +1,7 @@
 from models.main import db, User
 from models.segment import Exams
 from exception.validation_error import ValidationError
-from repository.reports import reports_exams_search_repository
+from repository import exams_repository
 from utils import dateutils, status
 from decorators.has_permission_decorator import has_permission, Permission
 
@@ -15,9 +15,7 @@ def get_exams(id_patient: int, user_context: User):
             status.HTTP_400_BAD_REQUEST,
         )
 
-    exams_list = reports_exams_search_repository.get_exams_by_patient(
-        id_patient, days=30
-    )
+    exams_list = exams_repository.get_exams_by_patient(id_patient, days=30)
     results = []
 
     for i in exams_list:
