2e0ce57a98991485d0246ca97c879e5ea1143f03
Marcelo Arocha <marceloarocha@gmail.com>
2025-01-28 21:35:55 -0300

    v4.28-beta

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from models.enums import NoHarmENV
 
 
 class Config:
-    VERSION = "v4.27-beta"
-    FRONTEND_VERSION = "4.0.20"
+    VERSION = "v4.28-beta"
+    FRONTEND_VERSION = "4.0.21"
     ENV = getenv("ENV") or NoHarmENV.DEVELOPMENT.value
     SECRET_KEY = getenv("SECRET_KEY") or "secret_key"
     API_KEY = getenv("API_KEY") or ""
