d383096c153e534eeb7c1f62ad74b1691baa26f7
Marcelo Arocha <marceloarocha@gmail.com>
2025-02-24 17:03:56 -0300

    report authorization

--- routes/reports/reports_config_rpt.py
+++ routes/reports/reports_config_rpt.py
@@ -1,3 +1,5 @@
+"""Route: Reports configuration route."""
+
 from flask import Blueprint
 
 from decorators.api_endpoint_decorator import api_endpoint
@@ -9,4 +11,5 @@ app_rpt_config = Blueprint("app_rpt_config", __name__)
 @app_rpt_config.route("/reports/config", methods=["GET"])
 @api_endpoint()
 def get_config():
+    """Get reports configuration."""
     return memory_service.get_reports()
