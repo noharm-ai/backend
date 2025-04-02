7215cb44d26c3598372ed5a0441e296296a0471b
Marcelo Arocha <marceloarocha@gmail.com>
2025-04-02 11:29:38 -0300

    test deploy

--- config.py
+++ config.py
@@ -24,9 +24,10 @@ class Config:
     REPORT_CONNECTION_STRING = (
         getenv("REPORT_CONNECTION_STRING") or "postgresql://postgres@localhost/noharm"
     )
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
-        minutes=int(getenv("JWT_ACCESS_TOKEN_EXPIRES", 2))
-    )
+    # JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+    #     minutes=int(getenv("JWT_ACCESS_TOKEN_EXPIRES", 2))
+    # )
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=1)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(
         days=int(getenv("JWT_REFRESH_TOKEN_EXPIRES", 30))
     )
