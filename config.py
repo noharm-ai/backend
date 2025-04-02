b78d60f59d1732649d440d4f9802bf3df98f2b36
Marcelo Arocha <marceloarocha@gmail.com>
2025-04-02 13:47:42 -0300

    test config

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config:
     # JWT_ACCESS_TOKEN_EXPIRES = timedelta(
     #     minutes=int(getenv("JWT_ACCESS_TOKEN_EXPIRES", 2))
     # )
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=1)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=2)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(
         days=int(getenv("JWT_REFRESH_TOKEN_EXPIRES", 30))
     )
