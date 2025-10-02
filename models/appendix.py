b949f81c50bb26c1d0cc03bda63be1e420cc296b
Marcelo Arocha <marceloarocha@gmail.com>
2025-10-02 09:58:07 -0300

    tp_prescalc config

--- models/appendix.py
+++ models/appendix.py
@@ -112,6 +112,7 @@ class SchemaConfig(db.Model):
     nh_care = db.Column("tp_noharm_care", db.Integer, nullable=False)
     cpoe = db.Column("cpoe", db.Boolean, nullable=False)
     return_integration = db.Column("integracao_retorno", db.Boolean, nullable=False)
+    tp_prescalc = db.Column("tp_prescalc", db.Integer, nullable=False)
 
     fl1 = db.Column("fl1_atualiza_indicadores_cpoe", db.Boolean, nullable=False)
     fl2 = db.Column("fl2_atualiza_indicadores_prescricao", db.Boolean, nullable=False)
