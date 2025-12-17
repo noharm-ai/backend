35c85987ec451a2c46742b3876b4838a7f715a22
Marcelo Arocha <marceloarocha@gmail.com>
2025-12-16 22:48:11 -0300

    Add Get Saved Queries functionality to reports

--- repository/reports/reports_repository.py
+++ repository/reports/reports_repository.py
@@ -1,5 +1,7 @@
 """Repository for managing reports."""
 
+from sqlalchemy import text
+
 from models.appendix import Report
 from models.main import db
 
@@ -18,3 +20,26 @@ def get_custom_reports(all: bool = False):
 def get_report(id_report: int):
     """Get single report by ID."""
     return db.session.query(Report).filter(Report.id == id_report).first()
+
+
+def get_saved_queries(schema: str) -> list[dict]:
+    """List saved queries."""
+
+    query = """
+        select
+            title,
+            sql_report
+        from
+            public.vanna_report
+        where
+            schema_report = :schema
+        order by
+            title
+    """
+
+    query_result = db.session.execute(text(query), {"schema": schema}).all()
+    results = []
+    for i in query_result:
+        results.append({"title": i.title, "sql_report": i.sql_report})
+
+    return results
