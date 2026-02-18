98caee94f7a64c1d23aae5e5b7738cc0c4d5ac2d
Marcelo Arocha <marceloarocha@gmail.com>
2026-02-17 22:24:52 -0300

    Add error handling for Lambda response in patient-day report service

--- services/reports/reports_consolidated_service.py
+++ services/reports/reports_consolidated_service.py
@@ -40,4 +40,14 @@ def get_patient_day_report(request_data: PatientDayReportRequest, user_context: 
         Payload=json.dumps(payload),
     )
 
-    return json.loads(response["Payload"].read().decode("utf-8"))
+    response_json = json.loads(response["Payload"].read().decode("utf-8"))
+
+    if isinstance(response_json, str):
+        response_json = json.loads(response_json)
+
+    if response_json.get("error", False):
+        raise Exception(
+            f"Consolidated patient-day report ERROR: {response_json.get('message', 'Erro inesperado. Consulte os logs')}"
+        )
+
+    return response_json
