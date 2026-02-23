152718a33b61c653ee967c9bd667e121b5f67499
Marcelo Arocha <marceloarocha@gmail.com>
2026-02-23 17:24:43 -0300

    reports consolidated prescription

--- services/reports/reports_consolidated_service.py
+++ services/reports/reports_consolidated_service.py
@@ -6,7 +6,10 @@ from config import Config
 from decorators.has_permission_decorator import Permission, has_permission
 from models.enums import NoHarmENV
 from models.main import User
-from models.requests.reports_consolidated_request import PatientDayReportRequest
+from models.requests.reports_consolidated_request import (
+    PatientDayReportRequest,
+    PrescriptionReportRequest,
+)
 
 
 @has_permission(Permission.READ_REPORTS)
@@ -48,6 +51,50 @@ def get_patient_day_report(request_data: PatientDayReportRequest, user_context: 
     if response_json.get("error", False):
         raise Exception(
             f"Consolidated patient-day report ERROR: {response_json.get('message', 'Erro inesperado. Consulte os logs')}"
+        )
+
+    return response_json
+
+
+@has_permission(Permission.READ_REPORTS)
+def get_prescription_report(request_data: PrescriptionReportRequest, user_context: User):
+    if Config.ENV == NoHarmENV.TEST.value:
+        return {}
+
+    payload = {
+        "command": "lambda_query_reports.get_prescription_report",
+        "schema": user_context.schema,
+        "year": request_data.year,
+        "id_department": request_data.id_department if request_data.id_department else None,
+        "segment": request_data.segment if request_data.segment else None,
+        "start_date": (
+            request_data.start_date.isoformat() if request_data.start_date else None
+        ),
+        "end_date": (
+            request_data.end_date.isoformat() if request_data.end_date else None
+        ),
+        "global_score_start": request_data.global_score_start,
+        "global_score_end": request_data.global_score_end,
+        "weekdays_only": request_data.weekdays_only,
+        "consider_empty_prescriptions": request_data.consider_empty_prescriptions,
+        "remove_prescription_at_discharge_date": request_data.remove_prescription_at_discharge_date,
+    }
+
+    lambda_client = boto3.client("lambda", region_name=Config.NIFI_SQS_QUEUE_REGION)
+    response = lambda_client.invoke(
+        FunctionName=Config.BACKEND_FUNCTION_NAME,
+        InvocationType="RequestResponse",
+        Payload=json.dumps(payload),
+    )
+
+    response_json = json.loads(response["Payload"].read().decode("utf-8"))
+
+    if isinstance(response_json, str):
+        response_json = json.loads(response_json)
+
+    if response_json.get("error", False):
+        raise Exception(
+            f"Consolidated prescription report ERROR: {response_json.get('message', 'Erro inesperado. Consulte os logs')}"
         )
 
     return response_json
