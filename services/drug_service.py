aef4fc5145c2fb7244b246e7769e6b9d956c18d3
Marcelo Arocha <marceloarocha@gmail.com>
2026-03-23 09:09:13 -0300

    fix: non existent record error

--- services/drug_service.py
+++ services/drug_service.py
@@ -679,9 +679,16 @@ def update_convert_factor(
 def get_drug_dashboard(id_drug: int, id_segment: int):
     """Get drug data for dashboard"""
 
-    drug, attributes, substance = drugs_repository.get_single_drug(
+    result = drugs_repository.get_single_drug(
         id_drug=id_drug, id_segment=id_segment
     )
+
+    if result is None:
+        raise ValidationError(
+            "Registro inexistente", "errors.invalidRecord", status.HTTP_400_BAD_REQUEST
+        )
+
+    drug, attributes, substance = result
 
     if drug is None:
         raise ValidationError(
