a509c2cb3f1d111c33040208565ab5359c5fbe82
Marcelo Arocha <marceloarocha@gmail.com>
2026-07-09 16:33:29 -0300

    feat: training modules wip

--- routes/training.py
+++ routes/training.py
@@ -2,29 +2,35 @@
 
 from flask import Blueprint, request
 
-from services import training_service
-from models.requests.training_request import TrainingItemFinishRequest
 from decorators.api_endpoint_decorator import api_endpoint
+from models.requests.training_request import TrainingItemFinishRequest
+from services import training_service
 
 app_training = Blueprint("app_training", __name__)
 
 
 @app_training.route("/training/list", methods=["GET"])
-@api_endpoint()
+@api_endpoint(
+    is_admin=True
+)  # TODO: remove is_admin=True when the training is available to all users
 def list_trainings():
     """List all active trainings"""
     return training_service.list_trainings()
 
 
 @app_training.route("/training/<int:id_training>/items", methods=["GET"])
-@api_endpoint()
+@api_endpoint(
+    is_admin=True
+)  # TODO: remove is_admin=True when the training is available to all users
 def list_training_items(id_training: int):
     """List all active items of a training"""
     return training_service.list_training_items(training_id=id_training)
 
 
 @app_training.route("/training/item/<int:id_training_item>/finish", methods=["POST"])
-@api_endpoint()
+@api_endpoint(
+    is_admin=True
+)  # TODO: remove is_admin=True when the training is available to all users
 def finish_training_item(id_training_item: int):
     """Register that the current user finished a training item"""
     return training_service.finish_training_item(
