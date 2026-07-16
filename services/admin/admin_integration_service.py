3d4b74e1f247dea9fdc9a0f923abc3dbfeea4cf5
Marcelo Arocha <marceloarocha@gmail.com>
2026-07-16 20:52:44 -0300

    chore: remove unused code

--- services/admin/admin_integration_service.py
+++ services/admin/admin_integration_service.py
@@ -7,7 +7,7 @@ from sqlalchemy import text
 from config import Config
 from decorators.has_permission_decorator import Permission, has_permission
 from exception.validation_error import ValidationError
-from models.appendix import InterventionReason, SchemaConfig, SchemaConfigAudit
+from models.appendix import SchemaConfigAudit
 from models.enums import SchemaConfigAuditTypeEnum
 from models.main import User, db
 from utils import aws, network_utils, status
@@ -30,37 +30,6 @@ def get_table_count(schema, table):
     result = db.session.execute(query, {"schemaname": schema, "table": table})
 
     return ([row[0] for row in result])[0]
-
-
-@has_permission(Permission.ADMIN_INTERVENTION_REASON)
-def init_intervention_reason(user_context: User):
-    """init motivointervencao table with records from test schema"""
-    schema = user_context.schema
-
-    if db.session.query(InterventionReason).count() > 0:
-        raise ValidationError(
-            "A tabela motivointervencao já está preenchida",
-            "errors.notSupported",
-            status.HTTP_400_BAD_REQUEST,
-        )
-
-    insert = text(
-        f"""
-            insert into {schema}.motivointervencao
-            (fkhospital, idmotivointervencao,nome, idmotivomae, ativo, suspensao, substituicao, tp_relacao, economia_customizada, bloqueante, ram)
-            select fkhospital, idmotivointervencao,nome, idmotivomae, ativo, suspensao, substituicao, tp_relacao, economia_customizada, bloqueante, ram
-            from hsc_test.motivointervencao
-        """
-    )
-
-    reset_seq = text(
-        f"""
-            SELECT setval('{schema}.motivointervencao_idmotivointervencao_seq', (SELECT max(idmotivointervencao) + 1 from hsc_test.motivointervencao), true);
-        """
-    )
-
-    db.session.execute(insert)
-    return db.session.execute(reset_seq)
 
 
 def _create_audit(
